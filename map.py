31da67e04bb13b4f1ea3a5248317524fb50e44db
Mia Jade Davis <miajadedavis@hotmail.co.uk>
2016-10-14 12:31:50 +0100

    changes to map

--- map.py
+++ map.py
@@ -1,12 +1,38 @@
 class Room:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, description):
+        self.description = ""
         self.exits = {}
         self.items = []
+        self.people = {}
 
 # sector building floor roomname
 
-IGY_hq_4_briefing2 = Room()
+rooms = {
+    "NAME" : {
+        "name" : "KNOWNNAME",
+        "unknown name" : "UNKNOWNNAME",
+        "description" : "DESCRIPTION",
+        "exits" : {}
+        }
+
+    "GYR_housingblocko_1_room6" : {
+        "name" : "Room 6",
+        "unknown name" : "Room 6",
+        "description" : "DESCRIPTION",
+        "exits" : {}
+        }
+    
+    "IGY_hq_4_briefing2" : {
+        "name" : "Briefing Room 2",
+        "unknown name" : "",
+        "description" : ""
+        },
+    
+    "IGY_hq_4_hallway" : {}
+    }
+
+"""IGY_hq_4_briefing2 = Room()
 IGY_hq_4_briefing2.exits = {"Hallway" : "IGY_hq_4_hallway"}
 
 IGY_hq_4_hallway = Room()
-IGY_hq_4_hallway.exits = {"Briefing Room 2" : "IGY_hq_4_briefing"}
+IGY_hq_4_hallway.exits = {"Briefing Room 2" : "IGY_hq_4_briefing"}"""
